3962017a1e7b82bf134eb38c58f9962c443f4252
Robert Ullman <bullman@nat-oitwireless-inside-vapornet100-c-10481.Princeton.EDU>
2015-04-03 22:24:19 -0400

    best response calculation now used parallel ForkJoinPool

--- Strategies.java
+++ Strategies.java
@@ -1,9 +1,13 @@
+import java.lang.Runtime;
+
 import java.util.List;
 import java.util.Map;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.*;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
 
 public class Strategies {
@@ -44,11 +48,13 @@ public class Strategies {
             strats.add(s);
         }
 
+        ForkJoinPool exec = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
+
         int numPlayers = gt.getGame().getNumPlayers();
         double[] epayoffs = new double[numPlayers];
         for (int p = 0; p < numPlayers; p++) {
             gt.reset();
-            epayoffs[p] = payoff(gt, gt.getRoot(), strats, p, false);
+            epayoffs[p] = exec.invoke(new PayoffTask(gt, gt.getRoot(), strats, p, false));
         }
         return epayoffs;
     }
@@ -59,23 +65,22 @@ public class Strategies {
         IterState state = new IterState(game.getNumPlayers());
         int iter = 0;
 
-        List<InfoSet> isets = Lists.newArrayList(gt.getInfoSets());
+        /*
+         *List<InfoSet> isets = Lists.newArrayList(gt.getInfoSets());
+         */
+
+        ForkJoinPool exec = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
 
         while (state.getMaxRegret() >= epsilon) {
             iter++;
-            s.average(response(gt, s, state), (1.0/(iter+1)));
-
-            InfoSet iset = isets.get(8);
-            /*
-             *System.out.println(iter + " " + state.getMaxRegret());
-             */
+            s.average(response(gt, s, state, exec), (1.0/(iter+1)));
             System.out.println(state.getMaxRegret());
         }
         System.out.println(iter + " iterations");
         return s;
     }
 
-    private static Strategy response(GameTree gt, Strategy s, IterState state) {
+    private static Strategy response(GameTree gt, Strategy s, IterState state, ForkJoinPool exec) {
         List<Strategy> strats = Lists.newArrayList();
         Strategy response = new Strategy(gt);
         for (int p = 1; p < gt.getGame().getNumPlayers(); p++) {
@@ -86,12 +91,12 @@ public class Strategies {
         for (int p = 0; p < gt.getGame().getNumPlayers(); p++) {
             gt.reset();
             strats.add(p, s);
-            double prePayoff = payoff(gt, gt.getRoot(), strats, p, false);
+            double prePayoff = exec.invoke(new PayoffTask(gt, gt.getRoot(), strats, p, false));
             strats.remove(p);
 
             gt.reset();
             strats.add(p, response);
-            double postPayoff = payoff(gt, gt.getRoot(), strats, p, true);
+            double postPayoff = exec.invoke(new PayoffTask(gt, gt.getRoot(), strats, p, true));
             strats.remove(p);
 
             assert postPayoff >= prePayoff;
@@ -102,94 +107,128 @@ public class Strategies {
         state.setMaxRegret(maxRegret);
         return response;
     }
+/*
+ *
+ *    private static double payoff(GameTree gt, GameNode n, List<Strategy> strats, int rplayer, boolean compBestResp) {
+ *        ForkJoinPool exec = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
+ *        Future<Double> future = exec.submit(new Callable<Double>() {
+ *            public Double call() throws Exception {
+ *                return payoff(gt, n, strats, rplayer, compBestResp, exec);
+ *            }
+ *        });
+ *        try {
+ *            return future.get();
+ *        } catch (Exception e) {
+ *            throw Throwables.propagate(e);
+ *        }
+ *    }
+ */
 
-    private static double payoff(GameTree gt, GameNode n, List<Strategy> strats, int rplayer, boolean compBestResp) {
-        if (n.getPayoff() != null) {
+    public static class PayoffTask extends RecursiveTask<Double> {
+        private final GameTree gt;
+        private final GameNode n;
+        private final List<Strategy> strats;
+        private final int rplayer;
+        private final boolean compBestResp;
+
+        public PayoffTask(GameTree gt, GameNode n, List<Strategy> strats, int rplayer, boolean compBestResp) {
+            this.gt = gt;
+            this.n = n;
+            this.strats = strats;
+            this.rplayer = rplayer;
+            this.compBestResp = compBestResp;
+        }
+
+        @Override
+        protected Double compute() {
+            if (n.getPayoff() != null) {
+                return n.getPayoff();
+            }
+
+            List<GameNode> children = n.getChildren();
+            if (children.size() == 0) {
+                return n.setPayoff(terminalPayoff(n, strats, rplayer, compBestResp));
+            }
+
+            InfoSet iset = n.getInfoSet();
+            PayoffTask[][] tasks = new PayoffTask[iset.size()][children.size()];
+            List<PayoffTask> allTasks = Lists.newArrayList();
+            double[][] cpayoffs = new double[iset.size()][children.size()];
+            double[] tpayoffs = new double[children.size()];
+            for (int i = 0; i < iset.size(); i++) {
+                GameNode in = iset.get(i);
+                List<GameNode> nchildren = in.getChildren();
+                for (int cindex = 0; cindex < nchildren.size(); cindex++) {
+                    GameNode child = nchildren.get(cindex);
+                    PayoffTask pt = new PayoffTask(gt, child, strats, rplayer, compBestResp);
+                    tasks[i][cindex] = pt;
+                    allTasks.add(pt);
+                }
+            }
+
+            invokeAll(allTasks);
+
+            for (int i = 0; i < tasks.length; i++) {
+                for (int cindex = 0; cindex < tasks[0].length; cindex++) {
+                    double pf = tasks[i][cindex].join();
+                    cpayoffs[i][cindex] += pf;
+                    tpayoffs[cindex] += pf;
+                }
+            }
+
+            int player = n.getPlayer();
+
+            // compute best action for a responding player
+            int aindex = -1;
+            if (player == rplayer && compBestResp) {
+                Preconditions.checkArgument(n instanceof ActionNode, "responding player must have actions");
+                ActionNode an = (ActionNode) n;
+                aindex = Arrays.maxarg(tpayoffs);
+                strats.get(player).setAction(iset, an.getBet(aindex));
+            }
+
+            // set payoffs for all nodes in the information set
+            for (int i = 0; i < iset.size(); i++) {
+                GameNode in = iset.get(i);
+                double payoff = (player == rplayer && compBestResp)  
+                    ? cpayoffs[i][aindex] : Arrays.sum(cpayoffs[i]); 
+                in.setPayoff(payoff);
+            }
+
             return n.getPayoff();
         }
 
-        List<GameNode> children = n.getChildren();
-        if (children.size() == 0) {
-            return n.setPayoff(terminalPayoff(n, strats, rplayer, compBestResp));
+        public double terminalPayoff(GameNode n, List<Strategy> strats, int player, boolean compBestResp) {
+            Preconditions.checkArgument(n instanceof PayoffNode, "terminal node must be a PayoffNode");
+            double payoff = ((PayoffNode) n).getPayoffForPlayer(player);
+            return freq(n, strats, player, compBestResp) * payoff;
         }
 
-        InfoSet iset = n.getInfoSet();
-        double[][] cpayoffs = new double[iset.size()][children.size()];
-        double[] tpayoffs = new double[children.size()];
-        for (int i = 0; i < iset.size(); i++) {
-            GameNode in = iset.get(i);
-            List<GameNode> nchildren = in.getChildren();
-            for (int cindex = 0; cindex < nchildren.size(); cindex++) {
-                GameNode child = nchildren.get(cindex);
-                double pf = payoff(gt, child, strats, rplayer, compBestResp);
-                cpayoffs[i][cindex] += pf;
-                tpayoffs[cindex] += pf;
+        private double freq(GameNode n, List<Strategy> strats, int rplayer, boolean compBestResp) {
+            if (n.getFreq() != null) {
+                return n.getFreq();
             }
-        }
 
-        int player = n.getPlayer();
-
-        // compute best action for a responding player
-        int aindex = -1;
-        if (player == rplayer && compBestResp) {
-            Preconditions.checkArgument(n instanceof ActionNode, "responding player must have actions");
-            ActionNode an = (ActionNode) n;
-            aindex = Arrays.maxarg(tpayoffs);
-            strats.get(player).setAction(iset, an.getBet(aindex));
-        }
-
-        // set payoffs for all nodes in the information set
-        for (int i = 0; i < iset.size(); i++) {
-            GameNode in = iset.get(i);
-            double payoff = (player == rplayer && compBestResp)  
-                ? cpayoffs[i][aindex] : Arrays.sum(cpayoffs[i]); 
-            in.setPayoff(payoff);
-        }
-
-        return n.getPayoff();
-    }
-
-    public static double terminalPayoff(GameNode n, List<Strategy> strats, int player, boolean compBestResp) {
-        Preconditions.checkArgument(n instanceof PayoffNode, "terminal node must be a PayoffNode");
-        double payoff = ((PayoffNode) n).getPayoffForPlayer(player);
-        return freq(n, strats, player, compBestResp) * payoff;
-    }
-
-    private static double freq(GameNode n, List<Strategy> strats, int rplayer, boolean compBestResp) {
-        if (n.getFreq() != null) {
-            return n.getFreq();
-        }
-
-        GameNode p = n.getParent();
-        int cindex = n.getChildIndex();
-        if (p == null) {
-            return 1.0;
-        }
-
-        double f = 1.0;
-        if (p instanceof DealNode) {
-            DealNode dn = (DealNode) p;
-            f = dn.getFreq(cindex);
-        } else if (p instanceof ActionNode) {
-            ActionNode an = (ActionNode) p;
-            if (an.getPlayer() != rplayer || !compBestResp) {
-                InfoSet iset = an.getInfoSet();
-                
-                /*
-                 *System.out.println(strats.get(an.getPlayer()));
-                 *System.out.println(iset + " " + an.getPlayer());
-                 */
-
-                /*
-                 *System.out.println(iset);
-                 *System.out.println(strats.get(an.getPlayer()));
-                 *System.out.println(an.getBet(cindex));
-                 *System.out.println(strats.get(an.getPlayer()).getProb(iset, an.getBet(cindex)));
-                 */
-                f = strats.get(an.getPlayer()).getProb(iset, an.getBet(cindex));
+            GameNode p = n.getParent();
+            int cindex = n.getChildIndex();
+            if (p == null) {
+                return 1.0;
             }
+
+            double f = 1.0;
+            if (p instanceof DealNode) {
+                DealNode dn = (DealNode) p;
+                f = dn.getFreq(cindex);
+            } else if (p instanceof ActionNode) {
+                ActionNode an = (ActionNode) p;
+                if (an.getPlayer() != rplayer || !compBestResp) {
+                    InfoSet iset = an.getInfoSet();
+                    f = strats.get(an.getPlayer()).getProb(iset, an.getBet(cindex));
+                }
+            }
+            return n.setFreq(freq(p, strats, rplayer, compBestResp) * f);
         }
-        return n.setFreq(freq(p, strats, rplayer, compBestResp) * f);
+
     }
 
 }
